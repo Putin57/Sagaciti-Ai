ae0896dc987b1ec01d95e35dbb981ef092cb68f8
Mahidul Islam <75845610+M4H1D@users.noreply.github.com>
2021-12-31 02:05:10 +0600

    Create question-14.py

---
+++ Assignment-1/Conditions/question-14.py
@@ -0,0 +1,11 @@
+# vowel & consonant checking 
+
+alp="AEIOUaeiou"
+
+x=input("Enter your one alphabet : ")
+
+if x in alp:
+
+	print("The alphabet is a vowel.")else:
+
+	print("The alphabet is a consonant.")
