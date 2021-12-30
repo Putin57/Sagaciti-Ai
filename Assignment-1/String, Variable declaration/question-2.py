4fed9f717ebaa5823e1d9dd5d79685aee73e0ab3
Mahidul Islam <75845610+M4H1D@users.noreply.github.com>
2021-12-31 02:03:05 +0600

    Add files via upload

---
+++ Assignment-1/String, Variable declaration/question-2.py
@@ -0,0 +1,4 @@
+#reverser name 
+x=input("Enter first name: ")
+y=input("Enter last name: ")
+print("Hello",y,x)
---
+++ Assignment-1/String, Variable declaration/question-4.py
@@ -0,0 +1,9 @@
+letter =input().lower()
+dict={}
+for x in letter:
+	keys=dict.keys()
+	if x in keys:
+		dict[x]+=1
+	else:
+		dict[x]=1
+print(dict)
