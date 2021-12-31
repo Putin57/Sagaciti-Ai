6710a975f17bfbd6121932833b5b448d2e819675
Mahidul Islam <75845610+M4H1D@users.noreply.github.com>
2021-12-31 20:43:03 +0600

    Add files via upload

---
+++ Python crash course/Assignment-1/Conditions/question-10.py
@@ -0,0 +1,6 @@
+#vote
+age=int(input("Enter your age : "))
+if age >=18:
+	print("Congratulation! You are eligible for casting your vote.")
+else:
+	print("Sorry! You aren't eligible for casting your vote.")
---
+++ Python crash course/Assignment-1/Conditions/question-14.py
@@ -0,0 +1,8 @@
+# vowel & consonant checking 
+
+alp="AEIOUaeiou"
+x=input("Enter your one alphabet : ")
+if x in alp:
+	print("The alphabet is a vowel.")
+else:
+	print("The alphabet is a consonant.")
